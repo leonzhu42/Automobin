b38ba81914b05a53704a560a6c06a839e404cf2e
leonzhu42 <leonzhu42@hotmail.com>
2015-01-07 19:28:52 +0800

    Added color image, depth image, skeleton image

--- Automobin/Automobin/MainWindow.xaml.cs
+++ Automobin/Automobin/MainWindow.xaml.cs
@@ -28,9 +28,25 @@ namespace Automobin
 	public partial class MainWindow : Window
 	{
 		private KinectSensor sensor;
-		private WriteableBitmap colorBitmap;
+		//Depth and color images
+		private WriteableBitmap depthColorBitmap;
+		private WriteableBitmap colorColorBitmap;
 		private DepthImagePixel[] depthPixels;
-		private byte[] colorPixels;
+		private byte[] depthColorPixels;
+		private byte[] colorColorPixels;
+		//Skeleton image
+		private const float RenderWidth = 640.0f;
+		private const float RenderHeight = 480.0f;
+		private const double JointThickness = 3;
+		private const double BodyCenterThickness = 10;
+		private const double ClipBoundsThickness = 10;
+		private readonly Brush centerPointBrush = Brushes.Blue;
+		private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
+		private readonly Brush inferredJointBrush = Brushes.Yellow;
+		private readonly Pen trackedBonePen = new Pen(Brushes.Green, 6);
+		private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+		private DrawingGroup drawingGroup;
+		private DrawingImage skeletonImage;
 
 		public MainWindow()
 		{
@@ -47,12 +63,22 @@ namespace Automobin
 				}
 			if(this.sensor != null)
 			{
+				this.drawingGroup = new DrawingGroup();
+				this.skeletonImage = new DrawingImage(this.drawingGroup);
 				this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+				this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+				this.sensor.SkeletonStream.Enable();
 				this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
-				this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
-				this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-				this.Image.Source = this.colorBitmap;
+				this.depthColorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+				this.colorColorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+				this.depthColorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+				this.colorColorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+				this.DepthImage.Source = this.depthColorBitmap;
+				this.ColorImage.Source = this.colorColorBitmap;
+				this.SkeletonImage.Source = this.skeletonImage;
 				this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+				this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+				this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
 				try
 				{
 					this.sensor.Start();
@@ -70,6 +96,22 @@ namespace Automobin
 		{
 			if (this.sensor != null)
 				this.sensor.Stop();
+		}
+
+		private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+		{
+			using(ColorImageFrame colorFrame = e.OpenColorImageFrame())
+			{
+				if(colorFrame != null)
+				{
+					colorFrame.CopyPixelDataTo(this.colorColorPixels);
+					this.colorColorBitmap.WritePixels(
+						new Int32Rect(0, 0, this.colorColorBitmap.PixelWidth, this.colorColorBitmap.PixelHeight),
+						this.colorColorPixels,
+						this.colorColorBitmap.PixelWidth * sizeof(int),
+						0);
+				}
+			}
 		}
 
 		private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
@@ -104,13 +146,13 @@ namespace Automobin
 						byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
 
 						// Write out blue byte
-						this.colorPixels[colorPixelIndex++] = intensity;
+						this.depthColorPixels[colorPixelIndex++] = intensity;
 
 						// Write out green byte
-						this.colorPixels[colorPixelIndex++] = intensity;
+						this.depthColorPixels[colorPixelIndex++] = intensity;
 
 						// Write out red byte                        
-						this.colorPixels[colorPixelIndex++] = intensity;
+						this.depthColorPixels[colorPixelIndex++] = intensity;
 
 						// We're outputting BGR, the last byte in the 32 bits is unused so skip it
 						// If we were outputting BGRA, we would write alpha here.
@@ -118,41 +160,224 @@ namespace Automobin
 					}
 
 					// Write the pixel data into our bitmap
-					this.colorBitmap.WritePixels(
-						new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
-						this.colorPixels,
-						this.colorBitmap.PixelWidth * sizeof(int),
+					this.depthColorBitmap.WritePixels(
+						new Int32Rect(0, 0, this.depthColorBitmap.PixelWidth, this.depthColorBitmap.PixelHeight),
+						this.depthColorPixels,
+						this.depthColorBitmap.PixelWidth * sizeof(int),
 						0);
 				}
 			}
 		}
 
-		private void ButtonScreenshotClick(object sender, RoutedEventArgs e)
+		private static void RenderClippedEdges(Skeleton skeleton, DrawingContext drawingContext)
 		{
-			if(this.sensor == null)
+			if (skeleton.ClippedEdges.HasFlag(FrameEdges.Bottom))
 			{
-				this.statusBarText.Text = Properties.Resources.ConnectDeviceFirst;
-				return;
+				drawingContext.DrawRectangle(
+					Brushes.Red,
+					null,
+					new Rect(0, RenderHeight - ClipBoundsThickness, RenderWidth, ClipBoundsThickness));
 			}
-			BitmapEncoder encoder = new PngBitmapEncoder();
-			encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
-			string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentCulture.DateTimeFormat);
-			string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-			string path = System.IO.Path.Combine(myPhotos, "KinectSnapshot-" + time + ".png");
 
-			try
+			if (skeleton.ClippedEdges.HasFlag(FrameEdges.Top))
 			{
-				using(FileStream fs = new FileStream(path, FileMode.Create))
-				{
-					encoder.Save(fs);
-				}
-				this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteSuccess, path);
+				drawingContext.DrawRectangle(
+					Brushes.Red,
+					null,
+					new Rect(0, 0, RenderWidth, ClipBoundsThickness));
 			}
-			catch(IOException)
+
+			if (skeleton.ClippedEdges.HasFlag(FrameEdges.Left))
 			{
-				this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteFailed, path);
+				drawingContext.DrawRectangle(
+					Brushes.Red,
+					null,
+					new Rect(0, 0, ClipBoundsThickness, RenderHeight));
+			}
+
+			if (skeleton.ClippedEdges.HasFlag(FrameEdges.Right))
+			{
+				drawingContext.DrawRectangle(
+					Brushes.Red,
+					null,
+					new Rect(RenderWidth - ClipBoundsThickness, 0, ClipBoundsThickness, RenderHeight));
 			}
 		}
-	}
 
+		private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+		{
+			Skeleton[] skeletons = new Skeleton[0];
+
+			using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+			{
+				if(skeletonFrame != null)
+				{
+					skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+					skeletonFrame.CopySkeletonDataTo(skeletons);
+				}
+			}
+
+			using(DrawingContext dc = this.drawingGroup.Open())
+			{
+				dc.DrawRectangle(Brushes.Black, null, new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+
+				if(skeletons.Length != 0)
+				{
+					foreach (Skeleton skel in skeletons)
+					{
+						RenderClippedEdges(skel, dc);
+						if (skel.TrackingState == SkeletonTrackingState.Tracked)
+							this.DrawBonesAndJoints(skel, dc);
+						else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
+							dc.DrawEllipse(
+								this.centerPointBrush,
+								null,
+								this.SkeletonPointToScreen(skel.Position),
+								BodyCenterThickness,
+								BodyCenterThickness);
+					}
+				}
+				this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+			}
+		}
+
+		private void DrawBonesAndJoints(Skeleton skeleton, DrawingContext drawingContext)
+		{
+			// Render Torso
+            this.DrawBone(skeleton, drawingContext, JointType.Head, JointType.ShoulderCenter);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderRight);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.Spine);
+            this.DrawBone(skeleton, drawingContext, JointType.Spine, JointType.HipCenter);
+            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipRight);
+
+            // Left Arm
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderLeft, JointType.ElbowLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.ElbowLeft, JointType.WristLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.WristLeft, JointType.HandLeft);
+
+            // Right Arm
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderRight, JointType.ElbowRight);
+            this.DrawBone(skeleton, drawingContext, JointType.ElbowRight, JointType.WristRight);
+            this.DrawBone(skeleton, drawingContext, JointType.WristRight, JointType.HandRight);
+
+            // Left Leg
+            this.DrawBone(skeleton, drawingContext, JointType.HipLeft, JointType.KneeLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.KneeLeft, JointType.AnkleLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.AnkleLeft, JointType.FootLeft);
+
+            // Right Leg
+            this.DrawBone(skeleton, drawingContext, JointType.HipRight, JointType.KneeRight);
+            this.DrawBone(skeleton, drawingContext, JointType.KneeRight, JointType.AnkleRight);
+            this.DrawBone(skeleton, drawingContext, JointType.AnkleRight, JointType.FootRight);
+ 
+            // Render Joints
+            foreach (Joint joint in skeleton.Joints)
+            {
+                Brush drawBrush = null;
+
+                if (joint.TrackingState == JointTrackingState.Tracked)
+                {
+                    drawBrush = this.trackedJointBrush;                    
+                }
+                else if (joint.TrackingState == JointTrackingState.Inferred)
+                {
+                    drawBrush = this.inferredJointBrush;                    
+                }
+
+                if (drawBrush != null)
+                {
+                    drawingContext.DrawEllipse(drawBrush, null, this.SkeletonPointToScreen(joint.Position), JointThickness, JointThickness);
+                }
+            }
+		}
+
+		private Point SkeletonPointToScreen(SkeletonPoint skelPoint)
+		{
+			DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skelPoint, DepthImageFormat.Resolution640x480Fps30);
+			return new Point(depthPoint.X, depthPoint.Y);
+		}
+
+		private void DrawBone(Skeleton skeleton, DrawingContext drawingContext, JointType jointType0, JointType jointType1)
+		{
+			Joint joint0 = skeleton.Joints[jointType0];
+			Joint joint1 = skeleton.Joints[jointType1];
+
+			// If we can't find either of these joints, exit
+			if (joint0.TrackingState == JointTrackingState.NotTracked ||
+				joint1.TrackingState == JointTrackingState.NotTracked)
+			{
+				return;
+			}
+
+			// Don't draw if both points are inferred
+			if (joint0.TrackingState == JointTrackingState.Inferred &&
+				joint1.TrackingState == JointTrackingState.Inferred)
+			{
+				return;
+			}
+
+			// We assume all drawn bones are inferred unless BOTH joints are tracked
+			Pen drawPen = this.inferredBonePen;
+			if (joint0.TrackingState == JointTrackingState.Tracked && joint1.TrackingState == JointTrackingState.Tracked)
+			{
+				drawPen = this.trackedBonePen;
+			}
+
+			drawingContext.DrawLine(drawPen, this.SkeletonPointToScreen(joint0.Position), this.SkeletonPointToScreen(joint1.Position));
+		}
+
+		private void ButtonScreenshotClick(object sender, RoutedEventArgs e)
+		{
+			//Save depth image
+			{
+				if (this.sensor == null)
+				{
+					this.statusBarText.Text = Properties.Resources.ConnectDeviceFirst;
+					return;
+				}
+				BitmapEncoder encoder = new PngBitmapEncoder();
+				encoder.Frames.Add(BitmapFrame.Create(this.depthColorBitmap));
+				string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentCulture.DateTimeFormat);
+				string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+				string path = System.IO.Path.Combine(myPhotos, "Depth " + time + ".png");
+
+				try
+				{
+					using (FileStream fs = new FileStream(path, FileMode.Create))
+					{
+						encoder.Save(fs);
+					}
+					this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteSuccess, path);
+				}
+				catch (IOException)
+				{
+					this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteFailed, path);
+				}
+			}
+			//Save color image
+			{
+				BitmapEncoder encoder = new PngBitmapEncoder();
+				encoder.Frames.Add(BitmapFrame.Create(this.colorColorBitmap));
+				string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentCulture.DateTimeFormat);
+				string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+				string path = System.IO.Path.Combine(myPhotos, "Color " + time + ".png");
+
+				try
+				{
+					using(FileStream fs = new FileStream(path, FileMode.Create))
+					{
+						encoder.Save(fs);
+					}
+					this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteSuccess, path);
+				}
+				catch(IOException)
+				{
+					this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteFailed, path);
+				}
+			}
+		}
+
+	}
 }
